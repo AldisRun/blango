ec9ce2ce83f86463f738343309010b155d666eeb
Aldis Ruņenieks <aldis.runenieks@icloud.com>
2023-07-16 11:49:21 +0000

    Finish django configurations

--- blango/settings.py
+++ blango/settings.py
@@ -1,126 +1,125 @@
-import os
+import os, dj_database_url
 import mimetypes
+from pathlib import Path
+from configurations import Configuration, values
+
 mimetypes.add_type("text/css", ".css", True)
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+class Dev(Configuration):
+  # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+  BASE_DIR = Path(__file__).resolve().parent.parent
 
-from pathlib import Path
+  # SECURITY WARNING: keep the secret key used in production secret!
+  SECRET_KEY = 'django-insecure-18my@44=5*gpmb1grcg#@1^6c5bsd6#r4pmln9z8k*rl2xpz2v'
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-18my@44=5*gpmb1grcg#@1^6c5bsd6#r4pmln9z8k*rl2xpz2v'
+  # SECURITY WARNING: don't run with debug turned on in production!
+  DEBUG = values.BooleanValue(True)
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
-X_FRAME_OPTIONS = 'ALLOW-FROM ' + os.environ.get('CODIO_HOSTNAME') + '-8000.codio.io'
-CSRF_COOKIE_SAMESITE = None
-CSRF_TRUSTED_ORIGINS = ['https://' + os.environ.get('CODIO_HOSTNAME') + '-8000.codio.io']
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SAMESITE = 'None'
-SESSION_COOKIE_SAMESITE = 'None'
-
-# Application definition
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'blog',
-    'crispy_forms',
-    'crispy_bootstrap5',
-]
-
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-#    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-#    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
-
-CRISPY_TEMPLATE_PACK = "bootstrap5"
-
-ROOT_URLCONF = 'blango.urls'
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-                # 'blango.context_processors.settings',      # Add
-                # 'social_django.context_processors.backends',  # Add
-                # 'social_django.context_processors.login_redirect', # Add
-            ],
-        },
-    },
-]
-
-WSGI_APPLICATION = 'blango.wsgi.application'
-
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+  ALLOWED_HOSTS = values.ListValue(["localhost", "0.0.0.0", ".codio.io"])
+  X_FRAME_OPTIONS = 'ALLOW-FROM ' + os.environ.get('CODIO_HOSTNAME') + '-8000.codio.io'
+  CSRF_COOKIE_SAMESITE = None
+  CSRF_TRUSTED_ORIGINS = ['https://' + os.environ.get('CODIO_HOSTNAME') + '-8000.codio.io']
+  CSRF_COOKIE_SECURE = True
+  SESSION_COOKIE_SECURE = True
+  CSRF_COOKIE_SAMESITE = 'None'
+  SESSION_COOKIE_SAMESITE = 'None'
 
 
-# Password validation
-# https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
+  # Application definition
+  INSTALLED_APPS = [
+      'django.contrib.admin',
+      'django.contrib.auth',
+      'django.contrib.contenttypes',
+      'django.contrib.sessions',
+      'django.contrib.messages',
+      'django.contrib.staticfiles',
+      'blog',
+      'crispy_forms',
+      'crispy_bootstrap5',
+  ]
+  MIDDLEWARE = [
+      'django.middleware.security.SecurityMiddleware',
+      'django.contrib.sessions.middleware.SessionMiddleware',
+      'django.middleware.common.CommonMiddleware',
+  #    'django.middleware.csrf.CsrfViewMiddleware',
+      'django.contrib.auth.middleware.AuthenticationMiddleware',
+      'django.contrib.messages.middleware.MessageMiddleware',
+  #    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+  ]
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
+  CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
-# Internationalization
-# https://docs.djangoproject.com/en/4.0/topics/i18n/
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
-USE_I18N = True
-USE_TZ = True
+  CRISPY_TEMPLATE_PACK = "bootstrap5"
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = 'static/'
+  ROOT_URLCONF = 'blango.urls'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+  TEMPLATES = [
+      {
+          'BACKEND': 'django.template.backends.django.DjangoTemplates',
+          'DIRS': [os.path.join(BASE_DIR, 'templates')],
+          'APP_DIRS': True,
+          'OPTIONS': {
+              'context_processors': [
+                  'django.template.context_processors.debug',
+                  'django.template.context_processors.request',
+                  'django.contrib.auth.context_processors.auth',
+                  'django.contrib.messages.context_processors.messages',
+                  # 'blango.context_processors.settings',      # Add
+                  # 'social_django.context_processors.backends',  # Add
+                  # 'social_django.context_processors.login_redirect', # Add
+              ],
+          },
+      },
+  ]
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+  WSGI_APPLICATION = 'blango.wsgi.application'
 
-# default static files settings for PythonAnywhere.
-#? see https://help.pythonanywhere.com/pages/DjangoStaticFiles for more info
-MEDIA_ROOT = '/home/workspace/blango/media'
-MEDIA_URL = '/media/'
-STATIC_ROOT = '/home/workspace/blango/static'
-STATIC_URL = '/static/'
+  # Database
+  # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+  DATABASES = {
+    "default": dj_database_url.config(default=f"sqlite:///{BASE_DIR}/db.sqlite3"),
+    "alternative": dj_database_url.config(
+        "ALTERNATIVE_DATABASE_URL",
+        default=f"sqlite:///{BASE_DIR}/alternative_db.sqlite3",
+    ),
+  }
+
+  # Password validation
+  # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
+  AUTH_PASSWORD_VALIDATORS = [
+      {
+          'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+      },
+      {
+          'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+      },
+      {
+          'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+      },
+      {
+          'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+      },
+  ]
+
+  # Internationalization
+  # https://docs.djangoproject.com/en/4.0/topics/i18n/
+  LANGUAGE_CODE = 'en-us'
+  TIME_ZONE = values.Value('UTC')
+  USE_I18N = True
+  USE_L10N = True
+  USE_TZ = True
+
+  # Static files (CSS, JavaScript, Images)
+  # https://docs.djangoproject.com/en/4.0/howto/static-files/
+  STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+  STATIC_URL = 'static/'
+
+  # Default primary key field type
+  # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+  DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+  CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+  CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+class Prod(Dev):
+  DEBUG = False
+  SECRET_KEY = values.SecretValue()
