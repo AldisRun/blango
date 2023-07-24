670c35d7e00d19ca8343b0dfb0d872359e1d595a
Aldis Ruņenieks <aldis.runenieks@icloud.com>
2023-07-24 15:07:30 +0000

    Finish caching

--- blog/views.py
+++ blog/views.py
@@ -3,14 +3,17 @@ from django.utils import timezone
 from blog.models import Post
 from blog.forms import CommentForm
 import logging
-
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_cookie
 
 logger = logging.getLogger(__name__)
 
+@cache_page(300)
+@vary_on_cookie
 def index(request):
-    posts = Post.objects.filter(published_at__lte=timezone.now()).exclude(published_at=None)
-    return render(request, "blog/index.html", {"posts": posts})
+    posts = Post.objects.filter(published_at__lte=timezone.now())
     logger.debug("Got %d posts", len(posts))
+    return render(request, "blog/index.html", {"posts": posts})
 
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
@@ -32,7 +35,5 @@ def post_detail(request, slug):
         comment_form = None
     
     return render(request, "blog/post-detail.html", {"post": post, "comment_form": comment_form})
-
-
 
 # Create your views here.
